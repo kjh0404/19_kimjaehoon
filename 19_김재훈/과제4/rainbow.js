05cea45a9c390651aa612e08d1ad4f76c01ad272
kjh0404 <113407661+kjh0404@users.noreply.github.com>
2023-05-21 23:40:12 +0900

    Add files via upload

---
+++ 19_김재훈/과제4/rainbow.js
@@ -0,0 +1,8 @@
+document.getElementById("r").style.color="red";
+document.getElementById("a").style.color="orange";
+document.getElementById("i").style.color="yellow";
+document.getElementById("n").style.color="green";
+document.getElementById("b").style.color="blue";
+document.getElementById("o").style.color="indigo";
+document.getElementById("w").style.color="violet";
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
